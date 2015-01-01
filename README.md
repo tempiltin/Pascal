e5b6d5b9ef4a3e2b648b6af2eb983caf5c3ab7c5
tempiltin <temurbekshukurov0707@gmail.com>
2015-01-01 12:00:00 +0500

    Commit on 2015-01-01T12:00:00

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Pascal
 Pascal
+program calculator;
+uses
+  Crt;
+
+var
+  choice: integer;
+
+procedure LaunchCalculator(choice: integer);
+var
+  number1, number2: integer;
+begin
+  write('Write first number: ');
+  readln(number1);
+  write('Write second number: ');
+  readln(number2);
+
+  case choice of
+    1: writeln(number1, ' + ', number2, ' = ', number1 + number2);
+    2: writeln(number1, ' - ', number2, ' = ', number1 - number2);
+    3: writeln(number1, ' * ', number2, ' = ', number1 * number2);
+    4: writeln(number1, ' / ', number2, ' = ', number1 / number2);
+  end;
+end;
+
+begin
+  repeat
+    writeln('1 - Add, 2 - Sub, 3 - Mul, 4 - Div, 5 - Quit');
+    readln(choice);
+    ClrScr;
+    if choice <> 5 then
+      LaunchCalculator(choice);
+  until choice = 5;
+  writeln('Thanks for using my calculator :D');
+end.
--- main.py
+++ main.py
@@ -2,52 +2,53 @@ import os
 import random
 import datetime
 
-def generate_pascal_file(file_name):
-    code_templates = [
-        "program HelloWorld; begin writeln('Hello, World!'); end.",
-        "program SumTwoNumbers; var a, b: integer; begin a := 5; b := 10; writeln(a + b); end.",
-        "program PrintNumbers; var i: integer; begin for i := 1 to 10 do writeln(i); end."
-    ]
+# Pascal papkasini yaratish
+if not os.path.exists("pascal"):
+    os.makedirs("pascal")
+
+# Pascal kod shablonlari
+CODE_TEMPLATES = [
+  
+    "program HelloWorld; begin writeln('Hello, World!'); end.",
+    "program SumTwoNumbers; var a, b: integer; begin a := 5; b := 10; writeln(a + b); end.",
+    "program PrintNumbers; var i: integer; begin for i := 1 to 10 do writeln(i); end.",
+    "program Factorial; var n, f, i: integer; begin n := 5; f := 1; for i := 1 to n do f := f * i; writeln(f); end.",
+    "program ReverseString; var s: string; begin s := 'Pascal'; writeln(copy(s, length(s), 1)); end."
+]
+
+# 1 yil davomida 70% kunni tanlab olish
+def get_commit_dates(year, activity_percent=70):
+    start_date = datetime.date(year, 1, 1)
+    end_date = datetime.date(year, 12, 31)
+    all_days = [(start_date + datetime.timedelta(days=i)) for i in range((end_date - start_date).days + 1)]
     
-    code = random.choice(code_templates)
+    # Tasodifiy kunlarni tanlash (70% faollik)
+    active_days = random.sample(all_days, int(len(all_days) * (activity_percent / 100)))
+    return sorted(active_days)
+
+# Pascal fayl yaratish
+def generate_pascal_file(file_name):
+    code = random.choice(CODE_TEMPLATES)
     with open(file_name, "w") as f:
         f.write(code)
 
-def get_commit_dates():
-    letters = {
-        "T": [(1, 3), (1, 4), (1, 5), (1, 6), (2, 5), (3, 5), (4, 5)],
-        "E": [(1, 3), (2, 3), (3, 3), (4, 3), (1, 4), (2, 4), (3, 4), (1, 5), (2, 5), (3, 5), (4, 5)],
-        "M": [(1, 3), (2, 3), (3, 3), (4, 3), (1, 4), (2, 4), (3, 4), (4, 4), (1, 5), (4, 5)],
-        "P": [(1, 3), (2, 3), (3, 3), (4, 3), (1, 4), (2, 4), (3, 4), (1, 5)],
-        "I": [(1, 3), (2, 3), (3, 3), (4, 3)],
-        "L": [(1, 3), (2, 3), (3, 3), (4, 3), (4, 4)],
-        "T": [(1, 3), (1, 4), (1, 5), (1, 6), (2, 5), (3, 5), (4, 5)],
-        "I": [(1, 3), (2, 3), (3, 3), (4, 3)],
-        "N": [(1, 3), (2, 3), (3, 3), (4, 3), (1, 4), (2, 5), (3, 6), (4, 6)]
-    }
-    
-    base_date = datetime.date(2015, 1, 1)
-    commit_dates = []
-    
-    for i, letter in enumerate("TEMPILTIN"):
-        for week, day in letters[letter]:
-            commit_dates.append(base_date + datetime.timedelta(weeks=week, days=day + i * 7))
-    
-    return commit_dates
-
+# Git commit va push qilish
 def commit_and_push(date):
     os.system("git add .")
-    commit_date = date.strftime("%Y-%m-%dT00:00:00")
+    commit_date = date.strftime("%Y-%m-%dT12:00:00")
     os.system(f'git commit --date="{commit_date}" -m "Commit on {commit_date}"')
     os.system("git push")
 
+# Asosiy funksiya
 def main():
-    commit_dates = get_commit_dates()
+    year = 2015  # O'zingiz xohlagan yilni tanlang
+    commit_dates = get_commit_dates(year)
+
     for date in commit_dates:
-        file_name = f"random_code_{date}.pas"
+        file_name = f"pascal/code_{date}.pas"
         generate_pascal_file(file_name)
         commit_and_push(date)
-        print(f"Committed and pushed for {date}")
+        print(f"✅ {date} uchun commit qilindi.")
 
 if __name__ == "__main__":
     main()
